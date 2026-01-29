cea38034321a59db99c2b0e1391b480a22f650f0
Gautam011004 <divyagautam326@gmail.com>
2026-01-29 22:26:07 +0530

    geyser stream updated, tls done

--- src/stream/client.rs
+++ src/stream/client.rs
@@ -5,6 +5,7 @@ use tonic::transport::{Channel, ClientTlsConfig};
 
 use crate::geyser::SubscribeRequest;
 use crate::geyser::geyser_client::GeyserClient;
+use crate::geyser::subscribe_update::UpdateOneof;
 
 pub struct GrpcClient {
     inner : GeyserClient<Channel>
@@ -28,7 +29,45 @@ impl GrpcClient {
         let mut stream = self.inner.subscribe(request_stream).await?.into_inner();
         println!("Connected to geyser client");
         while let Some(update) = stream.message().await? {
-            println!("Update: {:?}", update);
+            match update.update_oneof {
+                Some(UpdateOneof::Ping(_)) => {
+                    println!("ping");
+                }
+        
+                Some(UpdateOneof::Pong(_)) => {
+                    println!("pong");
+                }
+        
+                Some(UpdateOneof::Slot(slot)) => {
+                    println!("slot {}", slot.slot);
+                }
+        
+                Some(UpdateOneof::Block(block)) => {
+                    println!("block at slot {}", block.slot);
+                }
+        
+                Some(UpdateOneof::Transaction(tx)) => {
+                    println!("transaction update {:?}", tx);
+                }
+        
+                Some(UpdateOneof::Account(_)) => {
+                    println!("account update");
+                }
+        
+                Some(UpdateOneof::BlockMeta(meta)) => {
+                    println!("block meta {}", meta.slot);
+                }
+        
+                Some(UpdateOneof::Entry(_)) => {
+                    println!("entry update");
+                }
+        
+                Some(UpdateOneof::TransactionStatus(_)) => {
+                    println!("tx status update");
+                }
+        
+                None => {}
+            }
         }
         Ok(())
     }
